ac7ddb6183d8d55d50b19596ae6062d664911dec
jengaplus <jengaplus1@gmail.com>
2026-08-11 06:57:37 +0300

    Move splash to app.config.js and select available asset image dynamically

--- app.config.js
+++ app.config.js
@@ -1,14 +1,17 @@
-const appJson = require('./app.json');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = ({ config }) => {
+  const assetDir = path.resolve(__dirname, 'assets');
+  const candidateImages = ['jengaplus_enhanced.png', 'splash-icon.png'];
+  const splashImage = candidateImages.find((file) => fs.existsSync(path.join(assetDir, file)));
+
   return {
     ...config,
     expo: {
       ...config.expo,
       splash: {
-        // Change this path to use the splash image you want:
-        // './assets/jengaplus_enhanced.png' or './assets/splash-icon.png'
-        image: './assets/jengaplus_enhanced.png',
+        image: splashImage ? `./assets/${splashImage}` : './assets/splash-icon.png',
         resizeMode: 'contain',
         backgroundColor: '#ffffff',
       },
